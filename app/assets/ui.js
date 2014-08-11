0cf4170ab8aea674a2eb3da713dab2bde1116966
michael <michael@michaels-pc>
2014-08-10 23:44:10 -0500

    MMD v1.2 release

--- app/assets/ui.js
+++ app/assets/ui.js
@@ -93,35 +93,9 @@ function pasteHtmlAtCaret(html) {
 
 // Save Coded Document
 function saveTextAsHTML() {
-	var textToWrite = document.getElementById("code").value;
+	var textToWrite = editor.getValue();
 	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
 	var fileNameToSaveAs = "gen-layout.html";
-
-	var downloadLink = document.createElement("a");
-	downloadLink.download = fileNameToSaveAs;
-	downloadLink.innerHTML = "Download File";
-	if (window.webkitURL != null)
-	{
-		// Chrome allows the link to be clicked
-		// without actually adding it to the DOM.
-		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-	}
-	else
-	{
-		// Firefox requires the link to be added to the DOM
-		// before it can be clicked.
-		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-		downloadLink.onclick = destroyClickedElement;
-		downloadLink.style.display = "none";
-		document.body.appendChild(downloadLink);
-	}
-
-	downloadLink.click();
-}
-function saveTextAsProject() {
-	var textToWrite = document.getElementById("projsavecode").value;
-	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-	var fileNameToSaveAs = "mmd-layout.mmd";
 
 	var downloadLink = document.createElement("a");
 	downloadLink.download = fileNameToSaveAs;
@@ -147,22 +121,3 @@ function saveTextAsProject() {
 function destroyClickedElement(event) {
 	document.body.removeChild(event.target);
 }
-
-// Horizontal WYSIWYG Menu's Scrollbar
-(function() {
-    function scrollMenu(e) {
-		e = window.event || e;
-		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-		document.getElementById('idive').scrollLeft -= (delta*40); // Multiplied by 40
-		e.preventDefault();
-	}
-	if (document.getElementById('idive').addEventListener) {
-		// IE9, Chrome, Safari, Opera
-		document.getElementById('idive').addEventListener("mousewheel", scrollMenu, false);
-		// Firefox
-		document.getElementById('idive').addEventListener("DOMMouseScroll", scrollMenu, false);
-	} else {
-		// IE 6/7/8
-		document.getElementById('idive').attachEvent("onmousewheel", scrollMenu);
-	}
-})();
